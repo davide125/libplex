09d76d218cd31243a97d2d893fddef3d8867e5b5
Davide Cavalca <davide@geexbox.org>
2016-01-29 20:26:57 -0800

    initial import

---
+++ src/bin/plexq.c
@@ -0,0 +1,28 @@
+/*
+ * libplex: an interface to PleX REST API.
+ * Copyright (C) 2016 Davide Cavalca <davide@geexbox.org>
+ *
+ * This file is part of libplex.
+ *
+ * libplex is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * libplex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with libplex; if not, write to the Free Software
+ * Foundation, Inc, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <plex.h>
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+    return plex_global_init();
+}
